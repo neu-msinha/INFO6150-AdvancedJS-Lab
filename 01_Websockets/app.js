d05ebfcc2b8409cec4ba2e50d2fc87a2aa2af902
Mayukh Sinha <sinha.may@northeastern.edu>
2025-07-30 23:41:16 +0530

    01_Websockets: Add WebSocket Support

--- 01_Websockets/app.js
+++ 01_Websockets/app.js
@@ -3,9 +3,26 @@
 const express = require("express");
 const app = express();
 
-// Basic route to test server
+// Enable WebSocket support
+const wsExpress = require("express-ws")(app);
+
+// Basic HTTP route
 app.get("/", function (req, res) {
   res.send("Server is running!");
+});
+
+// WebSocket route
+app.ws("/chat/:roomName", function (ws, req, next) {
+  console.log(`New WebSocket connection to room: ${req.params.roomName}`);
+  
+  ws.on("message", function (data) {
+    console.log("Received:", data);
+    ws.send("Echo: " + data);
+  });
+
+  ws.on("close", function () {
+    console.log("WebSocket connection closed");
+  });
 });
 
 module.exports = app;
